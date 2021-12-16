a39f49ad12c174e233825f1994af311f9e843381
miuhuru <zermoun.yasmine@gmail.com>
2021-12-16 15:32:04 +0100

    completed getthetitles function by using the map method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
 const getTheTitles = function() {
-
+const thetitles = books.map(book => book.title)
+return thetitles
 };
-
+getTheTitles(books)
 // Do not edit below this line
 module.exports = getTheTitles;
